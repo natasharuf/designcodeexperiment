8194864d7cc9596e53a22f28cee4cff9e31b0294
Natasha Ruf <natasha.ruf@gmail.com>
2020-02-28 10:14:26 +0100

    github hochladen

---
+++ js/script.js
@@ -0,0 +1,8 @@
+Splitting({
+  /* target: String selector, Element, Array of Elements, or NodeList */
+  target: "[data-splitting]",
+  /* by: String of the plugin name */
+  by: "chars",
+  /* key: Optional String to prefix the CSS variables */
+  key: null
+});
